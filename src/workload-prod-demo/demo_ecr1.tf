e192e8ba1314c14e6d0cf2417ed6714af8f5c25f
Bryandc <bryandc@bizmoto.com.ph>
2026-05-12 15:13:12 +0800

    test ecr1

---
+++ src/workload-prod-demo/demo_ecr1.tf
@@ -0,0 +1,12 @@
+module "demo_ecr" {
+  source = "../../module/ecr"
+
+  name                 = "demo-app-ecr1"
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Project     = "Terraform-CICD-Demo"
+    Environment = "production"
+    ManagedBy   = "terraform"
+  }
+}
