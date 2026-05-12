37910c9c44a3f6ad86364d43d7a80a0cb3477163
Bryandc <bryandc@bizmoto.com.ph>
2026-05-12 22:19:45 +0800

    deploy s3 and dynamodb state backend first

--- src/workload-prod-demo/providers.tf
+++ src/workload-prod-demo/providers.tf
@@ -5,14 +5,6 @@ terraform {
       version = "~> 5.31.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "terraform-cicd-demo-state-bucket"
-    key            = "workload/prod-demo/terraform.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "terraform-cicd-demo-lock"
-    encrypt        = true
-  }
 }
 
 provider "aws" {
