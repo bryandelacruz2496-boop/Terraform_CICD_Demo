48aad3f51114f28a71f2d67de98b6e2918d09d8a
Bryandc <bryandc@bizmoto.com.ph>
2026-05-12 15:02:29 +0800

    fix: remove ecs references from outputs

--- src/workload-prod-demo/outputs.tf
+++ src/workload-prod-demo/outputs.tf
@@ -3,12 +3,7 @@ output "ecr_repository_url" {
   value       = module.demo_ecr.repository_url
 }
 
-output "ecs_cluster_name" {
-  description = "ECS cluster name"
-  value       = module.demo_ecs.cluster_name
-}
-
-output "ecs_service_name" {
-  description = "ECS service name"
-  value       = module.demo_ecs.service_name
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.demo_vpc.vpc_id
 }
