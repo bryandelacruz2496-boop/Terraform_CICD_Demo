94e06f206a371837673bda3f83dec863ad91a769
Bryandc <bryandc@bizmoto.com.ph>
2026-05-12 15:29:58 +0800

    fix: remove outputs referencing deleted modules

--- src/workload-prod-demo/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ecr_repository_url" {
-  description = "ECR repository URL"
-  value       = module.demo_ecr.repository_url
-}
-
-output "vpc_id" {
-  description = "VPC ID"
-  value       = module.demo_vpc.vpc_id
-}
